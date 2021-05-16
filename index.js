2fdeb7693d68ad12ca6e2e37f09715950a90e5aa
andychicken <83952965+andychicken@users.noreply.github.com>
2021-05-16 20:21:29 +0700

    git initial

--- index.js
+++ index.js
@@ -4,9 +4,4 @@ const port = 3000;
 
 app.get('/', (req, res) => res.send('Hello World!iiii'))
 
-var a=1;
-var b=2
-var c=a+b
-
-
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
